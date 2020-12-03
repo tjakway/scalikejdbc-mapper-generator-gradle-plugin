6c9710630c05e32cf0fd3c590446cd45c3ed76c4
Thomas Jakway <tjakway@nyu.edu>
2020-12-02 18:17:46 -0800

    integrating ScalikeJDBCMapperGenerator with ScalikeJDBCMapperGeneratorAdopter

--- src/main/scala/org/grimrose/gradle/scalikejdbc/ScalikeJDBCMapperGeneratorAdopter.scala
+++ src/main/scala/org/grimrose/gradle/scalikejdbc/ScalikeJDBCMapperGeneratorAdopter.scala
@@ -2,11 +2,16 @@ package org.grimrose.gradle.scalikejdbc
 
 import org.gradle.api.{InvalidUserDataException, Project}
 import org.grimrose.gradle.scalikejdbc.mapper.ScalikeJDBCMapperGenerator
+import org.grimrose.gradle.scalikejdbc.tasks.ScalikejdbcConfigTask
 
+import java.io.File
 import scala.util.control.Exception._
 
 
 class ScalikeJDBCMapperGeneratorAdopter(project: Project) {
+  import ScalikeJDBCMapperGeneratorAdopter._
+
+  import scala.collection.JavaConverters._
 
   val generator = new ScalikeJDBCMapperGenerator
 
@@ -38,8 +43,47 @@ class ScalikeJDBCMapperGeneratorAdopter(project: Project) {
 
   def targetOrDefaultDirectory(target: AnyRef, defaultPath: String) = allCatch.opt(project.file(target)).getOrElse(project.file(defaultPath))
 
+
+  def loadGen(scalikejdbcConfigTask: ScalikejdbcConfigTask,
+              srcDir: Option[AnyRef],
+              testDir: Option[AnyRef]) = {
+    val resolvedSrcDir = getDir(srcDir, getDefaultSrcDir)
+    val resolvedTestDir = getDir(testDir, getDefaultTestDir)
+
+    val resolveSettingsF =
+      if(scalikejdbcConfigTask.usePropertyFiles.isEmpty) {
+        generator.resolveSettingsWithDefaultPaths
+      } else {
+        generator.resolveSettingsWithPropertyFiles(
+          scalikejdbcConfigTask.usePropertyFiles.asScala.toSeq)
+      }
+
+    val (jdbcSettings, generatorSettings) =
+      resolveSettingsF(
+        project,
+        scalikejdbcConfigTask.jdbcConfig,
+        scalikejdbcConfigTask.generatorConfig,
+        ScalikejdbcConfigTask
+          .selectLoadPropertiesSetting(scalikejdbcConfigTask)
+      )
+
+
+  }
+
+  private def getDir(in: Option[AnyRef], getDefaultLoc: Project => File): File = {
+    in.map(_.toString).filter(_.trim.isEmpty).map(project.file) match {
+      case Some(found) => found
+      case None => getDefaultLoc(project)
+    }
+  }
 }
 
 object ScalikeJDBCMapperGeneratorAdopter {
   def apply(project: Project) = new ScalikeJDBCMapperGeneratorAdopter(project)
+
+  //TODO: look up from source sets in project
+  def getDefaultSrcDir(project: Project): File =
+    project.file("src/main/scala")
+  def getDefaultTestDir(project: Project): File =
+    project.file("src/test/scala")
 }
---
+++ src/main/scala/org/grimrose/gradle/scalikejdbc/tasks/ScalikejdbcConfigTask.scala
@@ -0,0 +1,52 @@
+package org.grimrose.gradle.scalikejdbc.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.{Input, Nested}
+import org.grimrose.gradle.scalikejdbc.interop
+import org.grimrose.gradle.scalikejdbc.interop.GeneratorConfig
+import org.grimrose.gradle.scalikejdbc.mapper.ScalikeJDBCMapperGenerator
+
+import java.io.File
+import java.util.{ArrayList, Collection}
+import scala.beans.BeanProperty
+
+
+
+class ScalikejdbcConfigTask extends DefaultTask {
+  @Input
+  @BeanProperty
+  @Nested
+  var jdbcConfig: interop.JdbcConfig = new interop.JdbcConfig()
+
+  @Input
+  @BeanProperty
+  @Nested
+  var generatorConfig: interop.GeneratorConfig = new GeneratorConfig()
+
+
+  @Input
+  @BeanProperty
+  var usePropertyFiles: Collection[File] = new ArrayList()
+
+  /**
+   * whether settings defined in this gradle task can silently override
+   * settings defined in mapper .properties files
+   * set to false to throw an exception instead
+   */
+  @Input
+  @BeanProperty
+  var allowOverridingPropertyFiles: Boolean = true
+}
+
+object ScalikejdbcConfigTask {
+  import ScalikeJDBCMapperGenerator.LoadPropertiesSetting
+  def selectLoadPropertiesSetting(in: ScalikejdbcConfigTask):
+    LoadPropertiesSetting = {
+
+    if(in.allowOverridingPropertyFiles) {
+      LoadPropertiesSetting.PreferGradle
+    } else {
+      LoadPropertiesSetting.AssertNoConflict
+    }
+  }
+}
