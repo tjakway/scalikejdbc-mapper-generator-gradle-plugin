fc64123197f2beee51a9f88bc9a833c39c0c861e
Thomas Jakway <tjakway@nyu.edu>
2021-03-18 15:55:45 -0700

    writing CombinedChecker

--- src/test/scala/org/grimrose/gradle/scalikejdbc/gen/sql/checks/CombinedChecker.scala
+++ src/test/scala/org/grimrose/gradle/scalikejdbc/gen/sql/checks/CombinedChecker.scala
@@ -1,5 +1,92 @@
 package org.grimrose.gradle.scalikejdbc.gen.sql.checks
 
-class CombinedChecker(val checkers: Set[OutputChecker]) {
+import org.grimrose.gradle.scalikejdbc.gen.sql.GenSQLException
 
+import java.io.File
+
+class CombinedChecker(val checkers: Set[OutputChecker])
+  extends OutputChecker {
+  import CombinedChecker._
+  override def apply(buildDir: File): OutputChecker.Result = {
+
+  }
+
+  private def perScalaFile(f: File): OutputChecker.Result = {
+    checkers.foldLeft(Map.empty: ResultMap) {
+      case (acc, thisChecker) => {
+
+      }
+    }
+  }
+}
+
+object CombinedChecker {
+  private type ResultMap = Map[OutputChecker, OutputChecker.Result]
+
+  /**
+   * TODO: implement fail-on-warning (probably pass an instance of test config)
+   * @param passed
+   * @param failed
+   */
+  private case class SortedResults(passed: ResultMap,
+                                   failed: ResultMap) {
+
+    private def updateMap(key: OutputChecker,
+                          res: OutputChecker.Result,
+                          target: String)
+                         (x: ResultMap): ResultMap = {
+      x.get(key) match {
+        case Some(existingRes) =>
+          throw DuplicateCheckError(key, res, existingRes, target)
+
+        case None => x.updated(key, res)
+      }
+    }
+
+    private def passed(res: OutputChecker.Result): Boolean =
+      res.noErrors
+
+    def update(thisChecker: OutputChecker,
+               thisRes: OutputChecker.Result,
+               target: String): SortedResults = {
+      def updateMapF = updateMap(thisChecker, thisRes, target)
+
+      if(passed(thisRes)) {
+        copy(passed = updateMapF(passed))
+      } else {
+        copy(failed = updateMapF(failed))
+      }
+    }
+
+    private def checkNotEmpty: Unit = {
+      if(passed.isEmpty && failed.isEmpty) {
+        throw NoCheckersError
+      } else {}
+    }
+  }
+
+  class CombinedCheckerError(override val msg: String)
+    extends GenSQLException(msg)
+
+  /**
+   * This is an exception instead of an error lodged in OutputChecker.Result
+   * because it indicates the orchestrating test code is the problem,
+   * not the generated SQL or scalikejdbc
+   * @param msg
+   */
+  case class DuplicateCheckError(override val msg: String)
+    extends CombinedCheckerError(msg)
+
+  object DuplicateCheckError {
+    def apply(checker: OutputChecker,
+              newRes: OutputChecker.Result,
+              prevRes: OutputChecker.Result,
+              target: String): DuplicateCheckError = {
+      DuplicateCheckError(s"Error running $checker on $target: " +
+        s"got $newRes but $prevRes already exists in ResultMap")
+    }
+  }
+
+  case object NoCheckersError extends CombinedCheckerError(
+    "Expected to run at least one OutputChecker")
 }
--- src/test/scala/org/grimrose/gradle/scalikejdbc/gen/sql/checks/OutputChecker.scala
+++ src/test/scala/org/grimrose/gradle/scalikejdbc/gen/sql/checks/OutputChecker.scala
@@ -3,6 +3,7 @@ package org.grimrose.gradle.scalikejdbc.gen.sql.checks
 import org.grimrose.gradle.scalikejdbc.gen.sql.GenSQLException
 
 import java.io.File
+import java.util.Formatter
 
 trait OutputChecker {
   def apply(buildDir: File): OutputChecker.Result
@@ -11,6 +12,7 @@ trait OutputChecker {
 object OutputChecker {
   import Result.{WarningType, ErrorType}
   case class Result(
+      header: Option[String],
       errors: Map[OutputChecker, Seq[ErrorType]],
       warnings: Map[OutputChecker, Seq[WarningType]]) {
 
@@ -56,13 +58,54 @@ object OutputChecker {
       }
     }
 
+    private def withFormatter(f: Formatter => Unit): String = {
+      val fmt: Formatter = new Formatter(new StringBuffer())
+      f(fmt)
+      fmt.toString.trim
+    }
+
+    private def printSeq[A](xs: Seq[A],
+                            indentation: String): String = {
+
+      withFormatter { fmt =>
+        xs.foreach(x => fmt.format("%s%s\n", indentation, x.toString))
+      }
+    }
+
+    private def defaultHeader(r: Result): String =
+      r.getClass.getName
+
+    private val subsectionIndentation: String = "\t"
+
+    def print(r: Result): String = {
+
+      withFormatter { fmt =>
+        fmt.format("%s\n%s\n%s\n",
+          defaultHeader(r),
+          printSeq(r.errors, subsectionIndentation)
+          )
+      }
+    }
+
     def combine(left: Result,
                 right: Result): Result = {
-
       Result(
+        mergeHeaders(left.header, right.header),
         errors = mergeMaps(left.errors, right.errors),
         warnings = mergeMaps(left.warnings, right.warnings)
       )
+    }
+
+    private def mergeHeaders(left: Option[String],
+                             right: Option[String]): Option[String] = {
+
+      (left, right) match {
+        case (None, None) => None
+        case _ => {
+          Some(s"Headers < " + left.getOrElse("None") + ", " +
+            right.getOrElse(None) + " >")
+        }
+      }
     }
   }
 }
